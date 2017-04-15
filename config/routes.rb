4406ff06683068ddb146da2f695b0b0cbdf2f055
Waihon Yew <yewwaihon@gmail.com>
2017-04-16 00:43:34 +0800

    Sets the routes constraints for the API

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # http://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: { format: :json } do
+    # We are going to list our resources here
+  end
 end
