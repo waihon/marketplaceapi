dca3174d3236cea74145b1cb9401521588636ff5
Waihon Yew <yewwaihon@gmail.com>
2017-04-16 11:25:56 +0800

    Adds first spec for user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryGirl.define do
   factory :user do
-    
+    # Note that there's no colon after a field's name
+    email FFaker::Internet.email
+    password "12345678"
+    password_confirmation "12345678"
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { @user = FactoryGirl.build(:user) }
+
+  subject { @user }
+
+  # is_expected is defined simply as expect(subject) and is designed for
+  # when you are using rspec-expectations with its newer expect-based syntax.
+  # https://relishapp.com/rspec/rspec-core/docs/subject/one-liner-syntax
+  it { is_expected.to respond_to(:email) }
+  it { is_expected.to respond_to(:password) }
+  it { is_expected.to respond_to(:password_confirmation) }
+
+  it { is_expected.to be_valid }
 end
