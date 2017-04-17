0d3b96a4421ad4785e54ee395d1492295e22970c
Waihon Yew <yewwaihon@gmail.com>
2017-04-17 10:31:04 +0800

    Adds create user endpoint

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe Api::V1::UsersController, type: :controller do
   describe "GET #show" do
     before(:each) do
       @user = FactoryGirl.create :user
+      #get :show, params: { id: @user.id }, format: :json
       get :show, params: { id: @user.id, format: :json }
     end
 
@@ -15,5 +16,42 @@ RSpec.describe Api::V1::UsersController, type: :controller do
     end
 
     it { is_expected.to respond_with 200 }
+  end
+
+  describe "POST #create" do
+    context "when is successfully created" do
+      before(:each) do
+        @user_attributes = FactoryGirl.attributes_for :user
+        post :create, params: { user: @user_attributes, format: :json }
+      end
+
+      it "renders the JSON representation for the user record just created" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:email]).to eql(@user_attributes[:email])
+      end
+
+      it { is_expected.to respond_with(201) } # Created
+    end
+
+    context "when is not created" do
+      before(:each) do
+        # Notice that email is not included
+        @invalid_user_attributes = { password: "12345678",
+                                     password_confirmation: "12345678" }
+        post :create, params: { user: @invalid_user_attributes, format: :json }
+      end
+
+      it "renders an errors JSON" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response).to have_key(:errors)
+      end
+
+      it "renders the JSON errors on why the user could not be created" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:errors][:email]).to include("can't be blank")
+      end
+
+      it { is_expected.to respond_with(422) } # Unprocessable Entity
+    end
   end
 end
