6de713d88101e4e9e1fb8586c2688bca2c2a1039
Waihon Yew <yewwaihon@gmail.com>
2017-04-18 12:25:44 +0800

    Sets Devise to respond to JSON explicitly

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  # From version 2.2.0, Devise no more respond to XML and JSON formats for
+  # sessions/new and registrations/new. Therefore we need to explicitly
+  # declare that we want it to respond to JSON.
+  # https://github.com/plataformatec/devise/issues/2260
+  # https://github.com/plataformatec/devise/issues/2209
+  # https://github.com/plataformatec/devise/issues/2215
+  respond_to :json
 end
