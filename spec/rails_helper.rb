ce9601474fa62b2b49727e5d5b935fdf7bbf11df
Waihon Yew <yewwaihon@gmail.com>
2017-04-19 08:37:43 +0800

    Refactors the JSON parse method

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -20,7 +20,7 @@ require "shoulda-matchers"
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 # Checks for pending migration and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
@@ -57,6 +57,7 @@ RSpec.configure do |config|
 
   # Test helpers
   config.include Devise::Test::ControllerHelpers, :type => :controller
+  config.include Request::JsonHelpers, :type => :controller
 end
 
 # https://www.sitepoint.com/learn-the-first-best-practices-for-rails-and-rspec/
