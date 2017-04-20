ed66ec301fc54226ce3727b982fc3d64b26c848a
Waihon Yew <yewwaihon@gmail.com>
2017-04-20 10:31:05 +0800

    Adds user authentication token

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,6 +11,7 @@ RSpec.describe User, type: :model do
   it { is_expected.to respond_to(:email) }
   it { is_expected.to respond_to(:password) }
   it { is_expected.to respond_to(:password_confirmation) }
+  it { is_expected.to respond_to(:auth_token) }
 
   it { is_expected.to be_valid }
 
@@ -26,4 +27,22 @@ RSpec.describe User, type: :model do
   it { is_expected.to validate_uniqueness_of(:email).ignoring_case_sensitivity }
   it { is_expected.to validate_confirmation_of(:password) }
   it { is_expected.to allow_value("example@domain.com").for(:email) }
+
+  it { is_expected.to validate_uniqueness_of(:auth_token) }
+
+  describe "#generate_authentication_token!" do
+    it "generates a unique token" do
+      #Devise.stub(:friendly_token).and_return("auniquetoken123")
+      #allow_any_instance_of(Devise).to receive(:friendly_token).and_return("auniquetoken123")
+      allow(Devise).to receive(:friendly_token).and_return("auniquetoken123")
+      @user.generate_authentication_token!
+      expect(@user.auth_token).to eql("auniquetoken123")
+    end
+
+    it "generates another token when one already has been taken" do
+      existing_user = FactoryGirl.create(:user, auth_token: "auniquetoken123")
+      @user.generate_authentication_token!
+      expect(@user.auth_token).not_to eql(existing_user.auth_token)
+    end
+  end
 end
